f3cdafe15f9ba75852aae8e8c3aeb7988b09cdf6
samet44 <sametkibriz44@gmail.com>
2021-05-15 21:37:08 +0300

    Added brand and company model

---
+++ src/app/models/brand.ts
@@ -0,0 +1,7 @@
+export interface Brand{
+    id:number;
+    brandName:String;
+    description_1:String;
+    description_2:String;
+    stockId:number;
+}
---
+++ src/app/models/company.ts
@@ -0,0 +1,11 @@
+export interface Company{
+    id:number;
+    companyName:String;
+    address:String;
+    tel1:Number;
+    tel2:Number;
+    fax:number;
+    taxNo:number;
+    taxName:String;
+    prpposalId:number;
+}
