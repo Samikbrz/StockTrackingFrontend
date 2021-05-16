728db6bf70653748a544e4be2a3c271a7afa4e41
samet44 <sametkibriz44@gmail.com>
2021-05-16 20:53:00 +0300

    Added single response model

---
+++ src/app/models/model.ts
@@ -0,0 +1,7 @@
+export interface Model{
+    id:Number;
+    modelName:String;
+    modelDefination:String
+    orginalBarcode:String;
+    stockId:Number;
+}
---
+++ src/app/models/productAcceptance.ts
@@ -0,0 +1,8 @@
+export interface ProductAcceptance{
+    id:number;
+    productResgistrationNumber:String;
+    acceptanceDate:Date;
+    userName:String;
+    invoiceNo:String
+    companyId:Number;
+}
