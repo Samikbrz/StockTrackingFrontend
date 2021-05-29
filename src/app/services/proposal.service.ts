1c93e53bed80122f08d906f1aaef4e276d372367
samet44 <sametkibriz44@gmail.com>
2021-05-29 22:21:08 +0300

    added other methods to proposal service

--- src/app/services/proposal.service.ts
+++ src/app/services/proposal.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { ProposalDetail } from '../models/proposalDetail';
+import { Proposal } from '../models/proposal';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +17,25 @@ export class ProposalService {
   getProposals() : Observable<ListResponseModel<ProposalDetail>>{
     let newPath=this.apiUrl+'/proposal/getdetail';
     return this.httpClient.get<ListResponseModel<ProposalDetail>>(newPath);     
+  }
+
+  getProposalById(modelId:number):Observable<ListResponseModel<ProposalDetail>>{
+    let newPath=this.apiUrl+"/proposal/getbyid?id="+modelId;
+    return this.httpClient.get<ListResponseModel<ProposalDetail>>(newPath);
+  }
+
+  update(proposal:Proposal):Observable<ResponseModel>{
+    let newPath=this.apiUrl+"/proposal/update";
+    return this.httpClient.post<ResponseModel>(newPath,proposal);
+  }
+
+  delete(proposal:Proposal):Observable<ResponseModel>{
+    let newPath=this.apiUrl+"/proposal/delete";
+    return this.httpClient.post<ResponseModel>(newPath,proposal);
+  }
+
+  add(proposal:Proposal):Observable<ResponseModel>{
+    let newPath=this.apiUrl+"/proposal/add";
+    return this.httpClient.post<ResponseModel>(newPath,proposal);
   }
 }
