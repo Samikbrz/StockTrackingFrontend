f1754e94b69c0c327fcd84336ae19c01c1c6ab61
samet44 <sametkibriz44@gmail.com>
2021-06-02 22:58:42 +0300

    stock store service added

---
+++ src/app/services/stock-store.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
+import { StockStore } from '../models/stockStore';
+import { StockStoreDetail } from '../models/stockStoreDetail';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockStoreService {
+
+  apiUrl = 'https://localhost:44359/api';
+  constructor(private httpClient: HttpClient) { }
+
+  getStockStores() : Observable<ListResponseModel<StockStoreDetail>>{
+    let newPath=this.apiUrl+'/stockstore/getdetail';
+    return this.httpClient.get<ListResponseModel<StockStoreDetail>>(newPath);     
+  }
+
+  getStockStoreById(stockStoreId:number):Observable<ListResponseModel<StockStoreDetail>>{
+    let newPath=this.apiUrl+"/stockstore/getbyid?id="+stockStoreId;
+    return this.httpClient.get<ListResponseModel<StockStoreDetail>>(newPath);
+  }
+
+  update(stockStore:StockStore):Observable<ResponseModel>{
+    let newPath=this.apiUrl+"/stockstore/update";
+    return this.httpClient.post<ResponseModel>(newPath,stockStore);
+  }
+
+  delete(stockStore:StockStore):Observable<ResponseModel>{
+    let newPath=this.apiUrl+"/stockstore/delete";
+    return this.httpClient.post<ResponseModel>(newPath,stockStore);
+  }
+
+  add(stockStore:StockStore):Observable<ResponseModel>{
+    let newPath=this.apiUrl+"/stockstore/add";
+    return this.httpClient.post<ResponseModel>(newPath,stockStore);
+  }
+}
