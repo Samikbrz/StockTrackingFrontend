ec7ecfeb166c0e49a62d264fa452a769fe171ba7
samet44 <sametkibriz44@gmail.com>
2021-06-05 20:07:13 +0300

    added delete product output  method

--- src/app/components/product-output/product-output.component.ts
+++ src/app/components/product-output/product-output.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { ProductOutput } from 'src/app/models/productOutput';
 import { ProductOutputDetail } from 'src/app/models/productOutputDetail';
 import { ProductOutputService } from 'src/app/services/product-output.service';
 
@@ -25,6 +26,16 @@ export class ProductOutputComponent implements OnInit {
       this.productOutputs=response.data
       this.toastrService.success(response.message,"Başarılı");
     })
+  }
+
+  
+  deleteProductOutput(productOutput:ProductOutput){
+    if(window.confirm("Emin misiniz?")){
+      this.productOutputService.delete(productOutput).subscribe(response=>{
+        this.toastrService.success(response.message,"Deleted")
+        window.location.reload();
+      })
+    }   
   }
 
 }
