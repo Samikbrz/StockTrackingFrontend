d9dd97ba435481e6a96bd95c45764c202a65b971
samet44 <sametkibriz44@gmail.com>
2021-06-06 13:27:58 +0300

    added store transfer delete method

--- src/app/components/store-transfer/store-transfer.component.ts
+++ src/app/components/store-transfer/store-transfer.component.ts
@@ -1,29 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { StoreTransfer } from 'src/app/models/storeTransfer';
 import { StoreTransferDetail } from 'src/app/models/storeTransferDetail';
 import { StoreTransferService } from 'src/app/services/store-transfer.service';
 
 @Component({
   selector: 'app-store-transfer',
   templateUrl: './store-transfer.component.html',
-  styleUrls: ['./store-transfer.component.css']
+  styleUrls: ['./store-transfer.component.css'],
 })
 export class StoreTransferComponent implements OnInit {
+  storeTransfers: StoreTransferDetail[];
 
-  storeTransfers:StoreTransferDetail[];
-
-  constructor(private storeTransferService:StoreTransferService,
-    private toastrService:ToastrService) { }
+  constructor(
+    private storeTransferService: StoreTransferService,
+    private toastrService: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.getStoreTransfers();
   }
 
   getStoreTransfers() {
-    this.storeTransferService.getStoreTransfers().subscribe(response=>{
-      this.storeTransfers=response.data;
-      this.toastrService.success(response.message,"Başarılı");
-    })
+    this.storeTransferService.getStoreTransfers().subscribe((response) => {
+      this.storeTransfers = response.data;
+      this.toastrService.success(response.message, 'Başarılı');
+    });
   }
 
+  deleteStoreTransfer(storeTransfer: StoreTransfer) {
+    if (window.confirm('Ürün transferini silmek istediğinizden emin misiniz?')) {
+      this.storeTransferService.delete(storeTransfer).subscribe((response) => {
+        window.location.reload();
+        this.toastrService.success('Silindi', 'Başarılı');
+      });
+    }
+  }
 }
